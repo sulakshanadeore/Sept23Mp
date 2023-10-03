6c0c21ed285c6f42ee0f66086b8ed9f65e2457c0
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-10-03 12:05:50 +0530

    FirstDayProgram

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+//assembly---multiple parts 
+namespace FirstDemo
+{
+
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ctrl + s
+            //Writeline method -- (parenthesis(parameters))
+
+            //Write----string-"", int--direct,decimal--d,float -f
+            //Console.Write("Hello,World");
+            //Console.Write("Hello India");
+            //Console.Write("Hello All");
+            //Console.WriteLine(1222);
+            //Console.WriteLine(1222342.455d);
+            //Console.WriteLine(12.345f);
+            //Console.WriteLine('A');
+            //Console.WriteLine(DateTime.Today);
+            //Console.WriteLine(true);
+            //Console.WriteLine(false);   
+            //ctrl KC---comment
+            //ctrl KU
+            int cnt=9;
+            Console.WriteLine($"The value of cnt= {cnt}");
+            float v = default(float);
+            Console.WriteLine($"The value of v={v}");   
+            string name=default(string);    
+            Console.WriteLine(name);
+
+            Console.Read();
+        }
+    }
+}
