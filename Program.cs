5c8a423a0af7e19c99730ed19a805b52ff6d346e
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-10-03 12:06:34 +0530

    SecondDayProgram

--- Program.cs
+++ Program.cs
@@ -1,37 +1,75 @@
 ﻿using System;
 
-//assembly---multiple parts 
-namespace FirstDemo
-{
 
-     class Program
+//Logical collection of interrelated classes,structures and enums.
+namespace CSharpBasics
+{
+    ////entity
+    //struct Book
+    //{
+    //    int bookid,string bookname,decimal price;
+    //    acceptbookdetails()
+    //    {
+
+
+    //    }
+    
+    //}
+
+
+    internal class Program
     {
         static void Main(string[] args)
         {
-            //Ctrl + s
-            //Writeline method -- (parenthesis(parameters))
+            Console.WriteLine($"Todays date ={DateTime.Now}");
 
-            //Write----string-"", int--direct,decimal--d,float -f
-            //Console.Write("Hello,World");
-            //Console.Write("Hello India");
-            //Console.Write("Hello All");
-            //Console.WriteLine(1222);
-            //Console.WriteLine(1222342.455d);
-            //Console.WriteLine(12.345f);
-            //Console.WriteLine('A');
-            //Console.WriteLine(DateTime.Today);
-            //Console.WriteLine(true);
-            //Console.WriteLine(false);   
-            //ctrl KC---comment
-            //ctrl KU
-            int cnt=9;
-            Console.WriteLine($"The value of cnt= {cnt}");
-            float v = default(float);
-            Console.WriteLine($"The value of v={v}");   
-            string name=default(string);    
-            Console.WriteLine(name);
+            Console.WriteLine("Enter birthdate");
+            DateTime birthdate=Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine(birthdate);
+            Console.WriteLine(birthdate.Year);
+            Console.WriteLine(birthdate.Month);
 
-            Console.Read();
+
+            //DateTime joiningDate = new DateTime(2023, 10, 03);
+            //Console.WriteLine(joiningDate);
+
+            //bool IsEmployee;
+            //Console.WriteLine("enter a boolean value, true for yes, false for no: R u a employee");
+            //IsEmployee = Convert.ToBoolean(Console.ReadLine());
+            //Console.WriteLine(IsEmployee);
+
+
+            //string empname;
+            //Console.WriteLine("enter name");
+            //empname=Console.ReadLine(); 
+            //Console.WriteLine(empname);
+            //Console.WriteLine("Enter a character");
+            //char c1=Convert.ToChar(Console.ReadLine());
+            //Console.WriteLine(c1);
+
+          //  Console.WriteLine("Enter First Number");
+            //1 byte--8 bits
+            //     int firstno =Convert.ToInt32(Console.ReadLine());
+          //  short firstno
+            //Single firstno =Convert.ToSingle(Console.ReadLine());
+           // Double firstno= Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Enter second number");
+            //int secondno= Convert.ToInt32(Console.ReadLine());
+            //            float secondno= Convert.ToSingle(Console.ReadLine());
+           // Double secondno = Convert.ToDouble(Console.ReadLine());
+            //int,float,decimal,string,char,datetime,bool
+            //struct,enum
+            //whenever you use struct/enum , its value stored on the stack,
+            //everything else is stored on the heap.-ie object of class-heap
+            //double ans = firstno + secondno;
+            //Console.WriteLine($"The answer of {firstno} + {secondno}={ans}");
+            Console.ReadLine();
+
+
+
+
+
         }
     }
 }
