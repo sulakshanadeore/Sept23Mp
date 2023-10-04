fa9bc78e35b231f5874404f7ceb326d1b7faa04e
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-10-04 12:52:42 +0530

    PersonClassOOPS

---
+++ Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsDemo//House
+{
+    //access specifier-class internal
+   internal  class Person
+    {
+        //private
+        internal static int personId=0;
+        private double _salary;
+        private string _firstname;
+        private int _deptno;
+        public void ShowPersonDetails()
+        {
+            personId = personId + 1;
+            Console.WriteLine($" Personid allotted to you = {personId}");
+            Console.WriteLine(_salary);
+            Console.WriteLine(_firstname);
+            Console.WriteLine(_deptno);
+        }
+
+
+        public void AcceptPersonDetails() 
+        {
+            Console.WriteLine("Enter person salary");
+            _salary=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine( "enter name");
+            _firstname=Console.ReadLine();
+            Console.WriteLine(  "enter deptno");
+            _deptno=Convert.ToInt32(Console.ReadLine());        
+
+        }
+
+
+
+
+        //private void PrivateMethodForPersonExampleMobile() {
+        //    Console.WriteLine("This is a private method");
+        //}
+        //public void UseChargerPublic() {
+        //    PrivateMethodForPersonExampleMobile();
+        //    Console.WriteLine("This is a public method");
+        //}
+
+        //internal void InternalMethodUtilityBox() {
+        //    PrivateMethodForPersonExampleMobile();
+        //    Console.WriteLine(  "Thsi is internal method");
+        //}
+
+    }
+}
---
+++ SecondProgamOOPs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsDemo
+{
+    //maintainabitlity --we write one class in one file
+    internal class SecondProgamOOPs
+    {
+        static void Main(string[] args)
+        {
+
+            Person p = new Person();//() construct the object called p of type Person
+            //           p.InternalMethodUtilityBox();
+            //p.UseChargerPublic();
+            p.AcceptPersonDetails();
+            p.ShowPersonDetails();
+            Console.WriteLine("---------------------");
+            Person p1=new Person();
+            p1.AcceptPersonDetails();
+            p1.ShowPersonDetails();
+            Console.WriteLine("---------------------");
+            Person p2=new Person();
+            p2.AcceptPersonDetails();
+            p2.ShowPersonDetails();
+
+       
+            Console.Read();
+
+        }
+    }
+}
