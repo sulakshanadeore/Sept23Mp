8f9231b6567a2562db97c7839f9ae4da25a98b00
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-10-04 11:33:22 +0530

    OOpsDemo-Call by value,reference,out parameters

--- Program.cs
+++ Program.cs
@@ -1,75 +1,114 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-//Logical collection of interrelated classes,structures and enums.
-namespace CSharpBasics
+namespace OOPsDemo
 {
-    ////entity
-    //struct Book
-    //{
-    //    int bookid,string bookname,decimal price;
-    //    acceptbookdetails()
-    //    {
-
-
-    //    }
-    
-    //}
-
-
     internal class Program
     {
-        static void Main(string[] args)
+        static void swapNos(ref int i,ref int j)
+        {            int temp = i;
+             i = j; j = temp;
+              Console.WriteLine($"Now the value of i={i}");
+            Console.WriteLine($"Now the value of j={j}");
+        }
+
+        static int CalculateValues(int i, int j, out int subans, out int divans)
         {
-            Console.WriteLine($"Todays date ={DateTime.Now}");
-
-            Console.WriteLine("Enter birthdate");
-            DateTime birthdate=Convert.ToDateTime(Console.ReadLine());
-            Console.WriteLine(birthdate);
-            Console.WriteLine(birthdate.Year);
-            Console.WriteLine(birthdate.Month);
-
-
-            //DateTime joiningDate = new DateTime(2023, 10, 03);
-            //Console.WriteLine(joiningDate);
-
-            //bool IsEmployee;
-            //Console.WriteLine("enter a boolean value, true for yes, false for no: R u a employee");
-            //IsEmployee = Convert.ToBoolean(Console.ReadLine());
-            //Console.WriteLine(IsEmployee);
+          int  addans = i + j;
+            if (j != 0)
+            {
+                subans = i - j;
+                divans = i / j;
+            }
+            else
+            {
+                subans = 0;
+                divans = 0;
+            }
+            return addans;
 
 
-            //string empname;
-            //Console.WriteLine("enter name");
-            //empname=Console.ReadLine(); 
-            //Console.WriteLine(empname);
-            //Console.WriteLine("Enter a character");
-            //char c1=Convert.ToChar(Console.ReadLine());
-            //Console.WriteLine(c1);
+        }
 
-          //  Console.WriteLine("Enter First Number");
-            //1 byte--8 bits
-            //     int firstno =Convert.ToInt32(Console.ReadLine());
-          //  short firstno
-            //Single firstno =Convert.ToSingle(Console.ReadLine());
-           // Double firstno= Convert.ToDouble(Console.ReadLine());
+                static void Calculate(int i, int j,out int addans,out int subans,out int divans)
+        {
+            addans = i + j;
+            if (j != 0)
+            {
+                subans = i - j;
+                divans = i / j;
+            }
+            else
+            {
+                subans = 0;
+                divans = 0;
+            }
+           }
+              static void Main(string[] args)
+        {  //BoxingUnboxing();
+            int a=10, b=20;
+            swapNos(ref a, ref b);
+            Console.WriteLine($"Now the value of a={a}");
+            Console.WriteLine($"Now the value of b={b}");
+            int addanswer;
+            Calculate(a, b, out  addanswer, out int subanswer, out int divanswer);
+            Console.WriteLine( addanswer);
+            Console.WriteLine(  subanswer);
+            Console.WriteLine(  divanswer);
+            Console.WriteLine(  "-------------------");
+            addanswer=CalculateValues(a, b, out int subans, out int divsionans);
+            Console.WriteLine( addanswer);
+            Console.WriteLine( divsionans);
+            Console.WriteLine(  subans);
 
-            //Console.WriteLine("Enter second number");
-            //int secondno= Convert.ToInt32(Console.ReadLine());
-            //            float secondno= Convert.ToSingle(Console.ReadLine());
-           // Double secondno = Convert.ToDouble(Console.ReadLine());
-            //int,float,decimal,string,char,datetime,bool
-            //struct,enum
-            //whenever you use struct/enum , its value stored on the stack,
-            //everything else is stored on the heap.-ie object of class-heap
-            //double ans = firstno + secondno;
-            //Console.WriteLine($"The answer of {firstno} + {secondno}={ans}");
-            Console.ReadLine();
+            Console.Read();
 
 
 
+        }
+
+        private static void BoxingUnboxing()
+        {
+            //Boxing & Unboxing
+            int counter = 10;
+            float i;
+            double d1;
+            decimal d2;
+            object o;
+            //Boxing
+            //(10)
+            o = counter;//bigger type is holding the value of smaller type
+                        //no data loss
+                        //value type(int,float,double,decimal--struct/enums)
+                        //only value types can be boxed 
+                        //Boxing means putting smaller type into bigger type.
+            Console.WriteLine("The data type of o=" + o.GetType());
+            //Unboxing means bringing it to its original type, by casting it.
+            counter = (int)o;
+            int a = counter + 100;
+            Console.WriteLine($"The addition ={a}");
+            Console.WriteLine("--------------");
+
+            string s = "Hello";
+            Console.WriteLine(s);
+            o = s;
+            Console.WriteLine(o);
+            Console.WriteLine(s);
+            //Casting
+            string s1 = (string)o;
+            Console.WriteLine(s1);
 
 
+
+            //object ---- biggest in the hierarchy of data types
+            //all data types inherit from object
+            //int,float etc are struct.
+            //Struct/enum we call it value types. and value types on stack.
+            //everything else is reference type.
+            //reference type are stored on the heap
         }
     }
 }
